200714eb421b5bbe2c985a8ef67647d9248619e2
floyddc <diegocoli68@gmail.com>
2025-10-08 11:20:24 +0200

    ignore

--- start-seed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start the Seed Node (GameWorld + GlobalView)
-# This MUST be started first!
-
-echo "🌱 Starting Seed Node..."
-sbt -Dakka.remote.artery.canonical.port=2551 "runMain it.unibo.agar.controller.SeedNode"
